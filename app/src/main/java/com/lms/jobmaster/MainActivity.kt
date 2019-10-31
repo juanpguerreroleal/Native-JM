1fa3e0e571a8e70d6fc7d7ef08518988c4e68b87
Oscar Alemán <fretshot@gmail.com>
2019-10-31 12:36:44 -0600

    Removed app bar and added the Fragment Selector.

--- app/src/main/java/com/lms/jobmaster/MainActivity.kt
+++ app/src/main/java/com/lms/jobmaster/MainActivity.kt
@@ -2,6 +2,8 @@ package com.lms.jobmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,26 @@ class MainActivity : AppCompatActivity() {
 
         val actionBar = supportActionBar
         actionBar!!.hide()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.home -> {
+                    Toast.makeText(applicationContext, "This is the home", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.map -> {
+
+                }
+
+                R.id.profile -> {
+
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
     }
 }
