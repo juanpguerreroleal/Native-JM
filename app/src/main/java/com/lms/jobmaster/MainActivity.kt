62297eb9cf5d37893fe67652dd05cefb1b863c5f
Oscar Alemán <fretshot@gmail.com>
2019-10-31 01:01:34 -0600

    added navigationItemBar and icon app

--- app/src/main/java/com/lms/jobmaster/MainActivity.kt
+++ app/src/main/java/com/lms/jobmaster/MainActivity.kt
@@ -2,11 +2,36 @@ package com.lms.jobmaster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.home -> {
+                    Toast.makeText(applicationContext, "This is the home", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.map -> {
+
+                }
+
+                R.id.profile -> {
+
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
     }
 }
