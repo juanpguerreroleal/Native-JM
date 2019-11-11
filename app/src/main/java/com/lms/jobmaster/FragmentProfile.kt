c7cf396b66bb1f65f0926932ff8cb1fbde19d448
Juan <juanpabloguleal@gmail.com>
2019-11-11 00:52:16 -0600

    Upload photo functionality added to profile fragment

--- app/src/main/java/com/lms/jobmaster/FragmentProfile.kt
+++ app/src/main/java/com/lms/jobmaster/FragmentProfile.kt
@@ -1,9 +1,15 @@
 package com.lms.jobmaster
 
+import android.app.Activity
 import android.app.AlertDialog
 import android.content.ContentValues.TAG
 import android.content.DialogInterface
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,26 +17,42 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
+import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.UploadTask
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_profile.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.lang.Exception
+import java.net.URL
 
+private const val READ_REQUEST_CODE: Int = 42
 class FragmentProfile: Fragment(){
     private lateinit var db: FirebaseFirestore
     private lateinit var auth: FirebaseAuth
+    private lateinit var storage: FirebaseStorage
     private lateinit var profile_string: String
     private lateinit var empty_string: String
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         auth = FirebaseAuth.getInstance()
+        storage = FirebaseStorage.getInstance()
     }
 
     override fun onStart() {
         super.onStart()
         edit_Button.setOnClickListener{editOrSaveEvent()}
         cancel_Button.setOnClickListener{cancelEvent()}
+        uploadPhoto_Button.setOnClickListener{uploadPhoto()}
+        uploadPhoto_Button.isClickable = false
         ArrayAdapter.createFromResource(
                 requireActivity(),
                 R.array.userTypes_array,
@@ -108,6 +130,7 @@ class FragmentProfile: Fragment(){
             occupancy_EditText.isEnabled = false
             phone_EditText.isEnabled = false
             userTypes_Spinner.isEnabled = false
+            uploadPhoto_Button.isClickable = false
             edit_Button.setText(R.string.edit)
             Toast.makeText(context ,R.string.text_changes_saved, Toast.LENGTH_SHORT).show()
         }
@@ -118,6 +141,7 @@ class FragmentProfile: Fragment(){
             experience_EditText.isEnabled = true
             occupancy_EditText.isEnabled = true
             phone_EditText.isEnabled = true
+            uploadPhoto_Button.isClickable = true
             cancel_Button.visibility = View.VISIBLE
             edit_Button.setText(R.string.save)
             Toast.makeText(context ,R.string.text_editable, Toast.LENGTH_SHORT).show()
@@ -132,6 +156,7 @@ class FragmentProfile: Fragment(){
         occupancy_EditText.isEnabled = false
         phone_EditText.isEnabled = false
         userTypes_Spinner.isEnabled = false
+        uploadPhoto_Button.isClickable = false
         edit_Button.setText(R.string.edit)
         Toast.makeText(context ,R.string.text_non_editable, Toast.LENGTH_SHORT).show()
     }
@@ -147,6 +172,7 @@ class FragmentProfile: Fragment(){
                 val occupancy = document.data?.get("occupancy").toString()
                 val phone = document.data?.get("phone").toString()
                 val userTypePosition = document.data?.get("userType").toString()
+                val photo = document.data?.get("photoUrl").toString()
                 if(fullname.isNotEmpty() and fullname.isNotBlank() and !fullname.equals("null")){
                     fullName_EditText.setText(document.data?.get("fullName").toString())
                 }
@@ -177,6 +203,9 @@ class FragmentProfile: Fragment(){
                 else{
                     phone_EditText.setText(empty_string)
                 }
+                if(photo.isNotEmpty() and photo.isNotBlank() and !photo.equals("null")){
+                    Picasso.get().load(Uri.parse(photo)).into(userPhoto_ImageView)
+                }
                 if(userTypePosition.isNotEmpty() and userTypePosition.isNotBlank() and !userTypePosition.equals("null")){
                     userTypes_Spinner.setSelection(userTypePosition.toInt())
                 }
@@ -197,6 +226,73 @@ class FragmentProfile: Fragment(){
             }
         }.addOnFailureListener{
             e -> Log.e(TAG, "Error")
+        }
+    }
+    private fun uploadPhoto(){
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        val currentUser = auth.currentUser
+        if (currentUser != null){
+            val uid = currentUser.uid
+            if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+                resultData?.data?.also { uri ->
+                    userPhoto_ImageView.setImageURI(uri)
+                    val storageRef = storage.reference
+                    var file = uri
+                    val imageRef = storageRef.child("UsersPhotos/${uid}.jpg")
+                    var uploadTask = imageRef.putFile(file)
+                    val urlTask = uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                        if (!task.isSuccessful) {
+                            task.exception?.let {
+                                throw it
+                            }
+                        }
+                        return@Continuation imageRef.downloadUrl
+                    }).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            val downloadUri = task.result.toString()
+                            uploadPhotoUrl(downloadUri)
+                        } else {
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+    private fun uploadPhotoUrl(url: String){
+        if(url.isNotEmpty() and !url.equals("null") and url.isNotBlank()){
+            val currentUser = auth.currentUser
+            if(currentUser != null){
+                db.collection(profile_string).document(currentUser.uid).get().addOnSuccessListener { document ->
+                    if(document != null){
+                        if(document.exists()){
+                            val profileObject = hashMapOf<String, Any>(
+                                    "photoUrl" to url
+                            )
+                            db.collection(profile_string).document(currentUser.uid).update(profileObject)
+                                    .addOnSuccessListener { Log.d(TAG, "Se actualizo el perfil correctamente") }
+                                    .addOnFailureListener { e -> Log.w(TAG, "Ocurrio un error en la actualizacion del perfil", e) }
+                            Log.d(TAG, "")
+                        }
+                        else{
+                            val profileObject = hashMapOf(
+                                    "uid" to currentUser.uid,
+                                    "photoUrl" to url
+                            )
+                            db.collection(profile_string).document(currentUser.uid).set(profileObject)
+                                    .addOnSuccessListener { Log.d(TAG, "Se registro el perfil correctamente") }
+                                    .addOnFailureListener { e -> Log.w(TAG, "Ocurrio un error en el registro del perfil", e) }
+                            Log.d(TAG, "")
+                        }
+                    }
+                }
+            }
         }
     }
 }
